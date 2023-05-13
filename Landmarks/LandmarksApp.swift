0ad832b58305fd50a36360faf4f4bc5bf41d328f
ChuanyuXue <chuanyu.xue@uconn.edu>
2023-05-13 19:39:59 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chuanyu on 2023/5/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
